6e0fe5a36e49438f687f3e60377994fa3b7cf5f1
KilianPeRu <162301542+KilianPeRu@users.noreply.github.com>
2024-05-20 18:19:19 +0200

    COMMIT AND PUSH KILIANO

--- src/clases/Habilidades.java
+++ src/clases/Habilidades.java
@@ -2,8 +2,8 @@ package clases;
 
 public class Habilidades {
     public int ID_HABILIDAD;
-    public String[] NOMBRE = new String[50];
-    public String[] EFECTO = new String[255];
+    public String NOMBRE;
+    public String EFECTO;
 
     public Habilidades(int ID_HABILIDAD) {
         this.ID_HABILIDAD = ID_HABILIDAD;
